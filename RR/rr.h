5de676f5094f08466eabff9e21e7d5201a9aa3d3
Felipe Soares <felipe31soares@gmail.com>
2015-11-28 10:26:39 -0200

    Alteração de Campos na struct e em parâmetros

--- RR/rr.h
+++ RR/rr.h
@@ -5,19 +5,34 @@
 #include <stdlib.h>
 #include <assert.h>
 
+
+
+typedef struct node{
+    int cost, pi, key;
+} node;
+
+typedef struct heap{
+    node * min_node;
+
+} heap;
+
 typedef struct edge {
-    int head_vertex, cost, tail_vertex, hot_line; // tail -> head
-    // hot_line = aresta faz parte do caminho mínimo
-    struct edge *next;
+    int head_vertex, cost, tail_vertex; // tail -> head
+    char hot_line; // hot_line = aresta faz parte do caminho mínimo
+    struct edge *next_pred; // prox na lista de predecessor
+    struct edge *next_adj;  // prox na lista de adjacentes
 } edge;
 
 typedef struct vertex {
     node heap_node;
-	int father, min_cost;
+	int pi, min_cost;
     int next_sp, key;
+    char mark;
     edge *adjacent;
     edge *predecessor;
 } vertex;
+
+
 
 /************************************************************************************************
 *************************************************************************************************
@@ -28,7 +43,7 @@ void rr_recalculate_shortest_path(vertex *graph, heap *queue);
 void rr_add_edge(vertex *graph, edge *edge_added);
 void rr_remove_edge(vertex *graph, edge *edge_removed);
 vertex *rr_mark_affected(vertex *graph, edge *edge_marked);
-rr_estimate_new_parents(vertex *graph, vertex *affected_list, heap *queue, vertex vertex);
+rr_estimate_new_parents(vertex *graph, vertex *affected_list, heap *queue, edge vertex);
 
 
 /************************************************************************************************
